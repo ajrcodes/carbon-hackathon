b58e0990102867a185334ca067d9129510961f1b
Josh <joshpeters52@gmail.com>
2017-06-08 09:20:21 -0400

    changed app stuff

--- carbonhackathon/FinalizedGroupViewController.swift
+++ carbonhackathon/FinalizedGroupViewController.swift
@@ -85,71 +85,83 @@ class FinalizedGroupViewController: UIViewController, UITableViewDelegate, UITab
     }
     
     func createGroupFromAPI(){
-        
-        let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
-        
-        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
-        loadingIndicator.hidesWhenStopped = true
-        loadingIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
-        loadingIndicator.startAnimating();
-        
-        alert.view.addSubview(loadingIndicator)
-        present(alert, animated: true, completion: nil)
-        
-        var people = [] as Array
-        for user in userGroup.users {
-            people.append([
-                "fname":user.firstName,
-                "lname":user.lastName,
-                "numbers":[
-                    ["type": "cell", "num": user.phoneNumber],
-                ],
-                ])
+        if(userGroup.users.count == 0){
+            let alert = UIAlertController(title: "Error", message: "No users are in group, you need to have at least 2 users", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
-        
-        
-        let dict = [
-            "group": people,
-            "note":textView.text,
-            "mms": switchy.isOn
-            ] as [String: Any]
-        
-        
-        let jsonData = try? JSONSerialization.data(withJSONObject: dict, options: [])
-        
-        
-        let url = URL(string: baseURL + "/api/send-group")!
-        var request = URLRequest(url: url)
-        
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "POST"
-        
-        // insert json data to the request
-        request.httpBody = jsonData
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                print("error on data")
-                print(error?.localizedDescription ?? "No data")
-                return
+        else if(userGroup.users.count == 1){
+            let alert = UIAlertController(title: "Error", message: "Only one user is in your group, you need add at least 2 users", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else{
+            let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+            
+            let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+            loadingIndicator.hidesWhenStopped = true
+            loadingIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+            loadingIndicator.startAnimating();
+            
+            alert.view.addSubview(loadingIndicator)
+            present(alert, animated: true, completion: nil)
+            
+            var people = [] as Array
+            for user in userGroup.users {
+                people.append([
+                    "fname":user.firstName,
+                    "lname":user.lastName,
+                    "numbers":[
+                        ["type": "cell", "num": user.phoneNumber],
+                    ],
+                    ])
             }
-            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
-            if let responseJSON = responseJSON as? [String: Any] {
-                if(responseJSON["success"] as! Bool){
-                    print("success")
-                    self.dismiss(animated: false, completion: nil)
+            
+            
+            let dict = [
+                "group": people,
+                "note":textView.text,
+                "mms": switchy.isOn
+                ] as [String: Any]
+            
+            
+            let jsonData = try? JSONSerialization.data(withJSONObject: dict, options: [])
+            
+            
+            let url = URL(string: baseURL + "/api/send-group")!
+            var request = URLRequest(url: url)
+            
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "POST"
+            
+            // insert json data to the request
+            request.httpBody = jsonData
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                guard let data = data, error == nil else {
+                    print("error on data")
+                    print(error?.localizedDescription ?? "No data")
+                    return
+                }
+                let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+                if let responseJSON = responseJSON as? [String: Any] {
+                    if(responseJSON["success"] as! Bool){
+                        print("success")
+                        self.dismiss(animated: false, completion: nil)
+                    }
+                    else{
+                        print("fail")
+                    }
                 }
                 else{
                     print("fail")
-                    self.dismiss(animated: false, completion: nil)
                 }
             }
-            else{
-                print("fail")
-                self.dismiss(animated: false, completion: nil)
-            }
+            task.resume()
+            
         }
-        task.resume()
+        
+        
 
         
     }
